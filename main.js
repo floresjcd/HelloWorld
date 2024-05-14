ed7d63a74a53d36dd73c77f1b4a395a5c5b8bc22
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2024-05-14 15:20:15 -0300

    Criação do main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// alert("Olá Mundo!")
+
+let nomeUsuario = "";
+let elemento = document.querySelector("#nome-usuário");
+
+while (nomeUsuario == "") {
+    nomeUsuario = prompt("Qual o seu nome?");
+}
+
+if (nomeUsuario == null) {
+    elemento.textContent = "seja muito bem-vindo.";
+} else {
+    elemento.textContent = nomeUsuario;
+}
+
+
